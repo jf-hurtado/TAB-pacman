2c62c0a67875282c9b4e08662cbc626c3cba081a
Jose <jfhv1@alu.ua.es>
2026-05-25 22:59:09 +0200

    feat: añadir comandos y videos ejecuciones

--- pacman.py
+++ pacman.py
@@ -743,7 +743,7 @@ def runGames(layout, pacman, ghosts, display, numGames, record, numTraining=0, c
 
     # Fijar semilla consistente
 
-    seed = '42'  # o cualquier valor fijo
+    seed = '62'  # o cualquier valor fijo
 
     random.seed(seed)
     ###################################################
